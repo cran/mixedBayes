1ae28ad819d50513321eb51c4a4d7f9860559bf1
Kun Fan <kfan@ksu.edu>
2026-03-16 19:40:14 +0000

    version 0.2.3

--- R/predict_mixedBayes.R
+++ R/predict_mixedBayes.R
@@ -4,7 +4,7 @@
 #' @param y a numeric vector of repeated-measure responses in long format.
 #'   The current version only supports continuous response.
 #' @param e the long-format design matrix for environment/treatment effects. In applications,
-#'   this is a set of dummy variables encoding treatment levels.
+#'   this is a group of dummy variables encoding treatment levels.
 #' @param X the long-format design matrix, including an intercept and optionally
 #'   time-related covariates.
 #' @param g the long-format matrix of genetic predictors.
@@ -26,10 +26,10 @@
 #' @examples
 #' data(data)
 #'
-#' fit <- mixedBayes(y, e, X, g, w, k, structure = c("bi-level"))
+#' fit <- mixedBayes(y, e, X, g, w, k, structure = "bi-level")
 #' pred1 <- predict_mixedBayes(fit, y, X, e, g, w, k, slope = TRUE, loss = "L1")
 #' print(pred1$pred_error)
-#' fit <- mixedBayes(y, e, X, g, w, k, robust =FALSE, structure = c("bi-level"))
+#' fit <- mixedBayes(y, e, X, g, w, k, robust =FALSE, quant =NULL,structure = "bi-level")
 #' pred2 <- predict_mixedBayes(fit, y, X, e, g, w, k, slope = TRUE, loss = "L2")
 #' print(pred2$pred_error)
 #'
@@ -37,14 +37,23 @@
 
 predict_mixedBayes <- function(object, y, X, e, g, w, k,
                                slope = TRUE,
-                               loss = c("L1", "L2")) {
-  if (k != as.integer(k)) {
-    stop("k must be an integer.")
+                               loss = "L1") {
+  if (length(k) != 1 || !is.numeric(k) || is.na(k) || k <= 0 || k %% 1 != 0) {
+    stop("k must be a positive integer.")
+  }
+  if (!is.logical(slope) || length(slope) != 1 || is.na(slope)) {
+    stop("slope must be TRUE or FALSE.")
   }
 
-  loss <- match.arg(loss)
-
   n  <- length(y)
+  if (n %% k != 0) {
+    stop("length(y) must be divisible by k.")
+  }
+
+  if (nrow(X) != n || nrow(e) != n || nrow(g) != n || nrow(w) != n) {
+    stop("X, e, g, and w must all have the same number of rows as length(y).")
+  }
+
   n1 <- n / k
 
   Zblock <- make_Zblock(k, n1, slope)
@@ -55,11 +64,13 @@ predict_mixedBayes <- function(object, y, X, e, g, w, k,
     w %*% object$coefficient$interaction +
     Zblock %*% as.vector(object$coefficient$random)
 
-  pred_error <- switch(
-    loss,
-    L1 = mean(abs(y - y_hat)),
-    L2 = mean((y - y_hat)^2)
-  )
+  if (loss == "L1") {
+    pred_error <- mean(abs(y - y_hat))
+  } else if (loss == "L2") {
+    pred_error <- mean((y - y_hat)^2)
+  } else {
+    stop("loss must be either 'L1' or 'L2'.")
+  }
 
   mixedBayes.pred <- list(
     y_hat = y_hat,
